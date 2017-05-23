6fa952486da182fdc8556ebefe9177640f3382c0
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2017-05-22 23:23:42 -0400

    Made ArticleMeta and its properties public, added JSON unmarshalling

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/ratanvarghese/tqtime"
+	"io/ioutil"
 	"strings"
 	"time"
 )
 
-type articleMeta struct {
-	src          string
-	url          string
-	pagetitle    string
-	articletitle string
-	publish      bool
-	date         string
-	topics       []string
+type ArticleMeta struct {
+	Src          string
+	URL          string
+	PageTitle    string
+	ArticleTitle string
+	Publish      bool
+	Date         string
+	Topics       []string
 }
 
 func main() {
 	t := time.Now()
 	long := tqtime.LongDate(t.Year(), t.YearDay())
 	longish := strings.Replace(long, "After Tranquility", "AT", 1)
-	fmt.Printf("%s\n", longish)
+	fmt.Println(longish)
 
 	gregorian := t.Format("Monday, 2 January, 2006 CE")
-	fmt.Printf("%s\n", gregorian)
+	fmt.Println(gregorian)
+
+	filename := "data.json"
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var aml []ArticleMeta
+	err = json.Unmarshal(data, &aml)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, element := range aml {
+		fmt.Println(element.ArticleTitle)
+	}
 }
