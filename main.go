0a970c285eb0a6a7a93ee83bc9cf834fcb161d9e
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2017-05-27 20:21:40 -0400

    Moved template code to template.go

--- main.go
+++ main.go
@@ -1,99 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
-	"github.com/ratanvarghese/tqtime"
-	"html/template"
-	"io/ioutil"
-	"log"
-	"os"
-	"strings"
-	"time"
 )
 
-type templateArgs struct {
-	applyTemplate *bool
-	templateFile  *string
-	contentFile   *string
-	articleTitle  *string
-	outputFile    *string
-	styleSheet    *string
-	date          *string
-}
-
-const gDateFormat = "2006-01-02"
-
 func main() {
-	var ta templateArgs
-	ta.applyTemplate = flag.Bool("apply", false, "Apply a template")
-	ta.templateFile = flag.String("template", "template.html", "File to use as a template")
-	ta.contentFile = flag.String("content", "content.html", "File with HTML content to insert in template")
-	ta.articleTitle = flag.String("title", "Untitled", "Title of the article")
-	ta.outputFile = flag.String("output", "index.html", "Filename of output of the executed template")
-	ta.styleSheet = flag.String("style", "../style.css", "Filename of stylesheet")
-	ta.date = flag.String("date", time.Now().Format(gDateFormat), "Gregorian date in format YYYY-MM-DD, defaults to today")
+	ta := makeTemplateArgs()
 
 	flag.Parse()
 
 	runTemplate(ta)
-}
-
-type articleExport struct {
-	Title       string
-	Stylesheet  string
-	Date        string
-	ContentHTML template.HTML
-}
-
-func killOnError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func printOnError(err error) {
-	if err != nil {
-		log.Print(err)
-	}
-}
-
-func runTemplate(ta templateArgs) {
-	if !*(ta.applyTemplate) {
-		return
-	}
-	t, err := template.ParseFiles(*(ta.templateFile))
-	killOnError(err)
-
-	content, err := ioutil.ReadFile(*(ta.contentFile))
-	killOnError(err)
-
-	f, err := os.Create(*(ta.outputFile))
-	killOnError(err)
-
-	var articleE articleExport
-	articleE.Title = *(ta.articleTitle)
-	articleE.Stylesheet = *(ta.styleSheet)
-
-	const outputGDateFormat = "Monday, 2 January, 2006 CE"
-	gDate, err := time.Parse(gDateFormat, *(ta.date))
-	killOnError(err)
-
-	tqDate := tqtime.LongDate(gDate.Year(), gDate.YearDay())
-	tqDateBetter := strings.Replace(tqDate, "After Tranquility", "AT", 1)
-	gDateStr := gDate.Format(outputGDateFormat)
-	articleE.Date = fmt.Sprintf("%s [Gregorian: %s]", tqDateBetter, gDateStr)
-
-	w := bufio.NewWriter(f)
-	articleE.ContentHTML = template.HTML(content)
-
-	err = t.Execute(w, articleE)
-	printOnError(err)
-
-	err = w.Flush()
-	printOnError(err)
-
-	err = f.Close()
-	printOnError(err)
 }
---
+++ template.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/ratanvarghese/tqtime"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type templateArgs struct {
+	applyTemplate *bool
+	templateFile  *string
+	contentFile   *string
+	articleTitle  *string
+	outputFile    *string
+	styleSheet    *string
+	date          *string
+}
+
+const gDateFormat = "2006-01-02"
+
+func makeTemplateArgs() templateArgs {
+	var ta templateArgs
+	ta.applyTemplate = flag.Bool("apply", false, "Run in TEMPLATE MODE")
+	ta.templateFile = flag.String("template", "template.html", "TEMPLATE MODE: File to use as a template")
+	ta.contentFile = flag.String("content", "content.html", "TEMPLATE MODE: File with HTML content to insert in template")
+	ta.articleTitle = flag.String("title", "Untitled", "TEMPLATE MODE: Title of the article")
+	ta.outputFile = flag.String("output", "index.html", "TEMPLATE MODE: Filename of output of the executed template")
+	ta.styleSheet = flag.String("style", "../style.css", "TEMPLATE MODE: Filename of stylesheet")
+	ta.date = flag.String("date", time.Now().Format(gDateFormat), "TEMPLATE MODE: Gregorian date in format YYYY-MM-DD, defaults to today")
+	return ta
+}
+
+type articleExport struct {
+	Title       string
+	Stylesheet  string
+	Date        string
+	ContentHTML template.HTML
+}
+
+func killOnError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printOnError(err error) {
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func runTemplate(ta templateArgs) {
+	if !*(ta.applyTemplate) {
+		return
+	}
+	t, err := template.ParseFiles(*(ta.templateFile))
+	killOnError(err)
+
+	content, err := ioutil.ReadFile(*(ta.contentFile))
+	killOnError(err)
+
+	f, err := os.Create(*(ta.outputFile))
+	killOnError(err)
+
+	var articleE articleExport
+	articleE.Title = *(ta.articleTitle)
+	articleE.Stylesheet = *(ta.styleSheet)
+
+	const outputGDateFormat = "Monday, 2 January, 2006 CE"
+	gDate, err := time.Parse(gDateFormat, *(ta.date))
+	killOnError(err)
+
+	tqDate := tqtime.LongDate(gDate.Year(), gDate.YearDay())
+	tqDateBetter := strings.Replace(tqDate, "After Tranquility", "AT", 1)
+	gDateStr := gDate.Format(outputGDateFormat)
+	articleE.Date = fmt.Sprintf("%s [Gregorian: %s]", tqDateBetter, gDateStr)
+
+	w := bufio.NewWriter(f)
+	articleE.ContentHTML = template.HTML(content)
+
+	err = t.Execute(w, articleE)
+	printOnError(err)
+
+	err = w.Flush()
+	printOnError(err)
+
+	err = f.Close()
+	printOnError(err)
+}
