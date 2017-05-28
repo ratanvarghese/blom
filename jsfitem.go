d21e4df048539508b002826c38b982681a187d48
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2017-05-28 14:08:52 -0400

    Made user write article path

--- jsfitem.go
+++ jsfitem.go
@@ -14,6 +14,7 @@ const itemMode = "item"
 
 type jsfItemArgs struct {
 	siteURL       *string
+	articlePath   *string
 	title         *string
 	contentFile   *string
 	datePublished *string
@@ -28,11 +29,12 @@ func makeJSFItem() (jsfItemArgs, *flag.FlagSet) {
 
 	j.siteURL = f1.String("siteurl", "http://ratan.blog", "Your website URL")
 	j.title = f1.String("title", "Untitled", "The title of the article")
+	j.articlePath = f1.String("folder", "untitled", "The path of the article relative to your website")
 	j.contentFile = f1.String("content", "content.html", "File with the article content")
 	j.datePublished = f1.String("pdate", todayYYYYMMDD(), "Publish date (gregorian) in YYYY-MM-DD format")
 	j.dateModified = f1.String("mdate", todayYYYYMMDD(), "Modify date (gregorian) in YYYY-MM-DD format")
 	j.tags = f1.String("tags", "", "Comma-seperated tags")
-	j.attachments = f1.String("attach", "", "Files to attach")
+	j.attachments = f1.String("attach", "", "Comma-seperated files to attach")
 
 	return j, f1
 }
@@ -56,8 +58,7 @@ type jsfItem struct {
 func buildItem(ja jsfItemArgs) {
 	var ji jsfItem
 	ji.Title = *(ja.title)
-	cleanTitle := strings.Replace(strings.ToLower(ji.Title), " ", "-", -1)
-	ji.ID = path.Join(*(ja.siteURL), cleanTitle)
+	ji.ID = path.Join(*(ja.siteURL), *(ja.articlePath))
 	ji.URL = ji.ID
 	ji.Tags = strings.Split(*(ja.tags), ",")
 
