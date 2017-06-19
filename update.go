5620b20a9a98effc3cb433ada2db66569518b903
Ratan Varghese <19657319+ratanvarghese@users.noreply.github.com>
2017-06-18 22:08:40 -0400

    Added xupdate.go

--- update.go
+++ update.go
@@ -31,22 +31,6 @@ type jsfMain struct {
 	Items       []jsfItem `json:"items"`
 }
 
-type byDatePublished []jsfItem
-
-func (b byDatePublished) Len() int {
-	return len(b)
-}
-
-func (b byDatePublished) Swap(i, j int) {
-	b[i], b[j] = b[j], b[i]
-}
-
-func (b byDatePublished) Less(i, j int) bool {
-	ti, _ := time.Parse(time.RFC3339, b[i].DatePublished)
-	tj, _ := time.Parse(time.RFC3339, b[j].DatePublished)
-	return ti.After(tj)
-}
-
 func noFrillsArticle() articleArgs {
 	var args articleArgs
 	template := defaultTemplate
